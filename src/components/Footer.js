c95d9dd2a898cec5920de1d7ec0565ebb77b87ad
Chris Dietrich <chrisdietrich3666@gmail.com>
2021-05-31 19:28:59 -0500

    added icons and improved footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import "../styles/components/GetInTouch.scss"
 
 export default function GetInTouch() {
+
+    // https://www.youtube.com/watch?v=kjg0ZUDjRmQ&ab_channel=OkayDexterOkayDexter
     return (
         <div className="get-in-touch-container">
             <h2>Get In Touch</h2>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,54 +1,87 @@
-import GetInTouch from "../components/GetInTouch"
+import { FaReact, FaBootstrap, FaHtml5, FaCss3Alt, FaSass, FaGithubSquare } from "react-icons/fa"
+import { IoLogoJavascript } from "react-icons/io"
+import { SiVisualstudio, SiFirebase, SiPostman } from "react-icons/si"
+import GetInTouch from "../components/Footer"
 import "../styles/components/Home.scss"
 
+//https://mail.google.com/mail/u/0/#inbox?projector=1
+//ADD A HERO IMAGE A PICTURE OF ME AT THE COMPUTER FROM BEHIND SHOWING MY SILHOUETTE 
 export default function Home() {
     return (
     <div className="home-container"> 
         <h1>Successful Front-end Development</h1>
-        <h3> Hello I'm Chris, a Front-end Developer who is passionate about web development and who is a lifelong learner</h3>
+        <h3> Hello I'm Chris, a Front-end Developer with a true passion for web development</h3>
         
         
         <div className="container">
             <div className="row">
                 <div className="col">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>
+                        {/* <img src="..." className="card-img-top" alt="..."/> */}
                         <div className="card-body">
                             <h5 className="card-title">Languages</h5>
-                            <ul>
-                                <li>Javascript</li>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>Sass</li>
-                            </ul>
+                            
+                            <div className="credentials">
+                                <IoLogoJavascript/>
+                                <p>Javascript</p>
+                            </div>
+                            <div className="credentials">
+                                <FaHtml5/>
+                                <p>HTML</p>
+                            </div>
+                            <div className="credentials">
+                                <FaCss3Alt/>
+                                <p>CSS</p>
+                            </div>
+                            <div className="credentials">
+                                <FaSass/>
+                                <p>Sass</p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 <div className="col">
                 <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>
+                        {/* <img src="..." className="card-img-top" alt="..."/> */}
                         <div className="card-body">
                             <h5 className="card-title">Frameworks</h5>
-                            <ul>
-                                <li>React</li>
-                                <li>Bootstrap</li>
-                            </ul>
+                            <div className="credentials">
+                                <FaReact/>
+                                <p>React</p>
+                            </div>
+                            <div className="credentials">
+                                <FaBootstrap/>
+                                <p>Bootstrap</p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 <div className="col">
                 <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>
+                        {/* <img src="..." className="card-img-top" alt="..."/> */}
                         <div className="card-body">
                             <h5 className="card-title">Other Stuff</h5>
-                            <ul>
-                                <li>Github</li>
-                                <li>VSC</li>
-                                <li>VSC integrated terminal</li>
-                                <li>Firebase</li>
-                            </ul>
+                            <div className="credentials">
+                                <FaGithubSquare/>
+                                <p>Github</p>
+                            </div>
+                            <div className="credentials">
+                                <SiVisualstudio/>
+                                <p>VSC</p>
+                            </div>
+                            <div className="credentials">
+                                <SiFirebase/>
+                                <p>Firebase</p>
+                            </div>
+                            <div className="credentials">
+                                <SiPostman/>
+                                <p>Postman</p>
+                            </div>
+                            <p>VSC integrated terminal</p>
+                            <p>Axios</p>
+        
                         </div>
                     </div>
                 </div>
@@ -60,16 +93,34 @@ export default function Home() {
             <div class="row">
                 <div class="col-4">
                 <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">English in Eight Minutes</h5>
+                            <p className="card-text">So this is how it started. 
+                            I was building a English teaching website with the help of some students.
+                            I quickly became fascinated with coding and next realized I had found my
+                            first real professinal passion.</p>
                         </div>
                     </div>
                 </div>
+              
                 <div class="col-8">
-                    Column
+                    <img src="../../images/english-in-eight.png" alt="" />
                 </div> 
+            </div>
+        </div>
+        <div class="container last-container">
+            <div class="row">
+                <div class="col-8">
+                    <img src="../../images/dietrich-landcare.png" alt="" />
+                </div>
+                <div class="col-4">
+                <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Dietrich Landcare</h5>
+                            <p className="card-text">My son needed a website for his business, so I built him one</p>
+                        </div>
+                    </div>
+                </div>  
             </div>
         </div>
         <GetInTouch/>
