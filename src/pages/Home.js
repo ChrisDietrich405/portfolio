92a888d5ab3b2d78e8925b5eac22683144a1d1bc
Chris Dietrich <chrisdietrich3666@gmail.com>
2021-06-19 15:43:47 -0500

    linking project websites to portfolio images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom"
+import { Image } from "react"
+
 import { FaReact, FaBootstrap, FaHtml5, FaCss3Alt, FaSass, FaGithubSquare } from "react-icons/fa"
 import { IoLogoJavascript } from "react-icons/io"
 import { SiVisualstudio, SiFirebase, SiPostman } from "react-icons/si"
 import GetInTouch from "../components/Footer"
 import "../styles/components/Home.scss"
+
 
 //https://mail.google.com/mail/u/0/#inbox?projector=1
 //ADD A HERO IMAGE A PICTURE OF ME AT THE COMPUTER FROM BEHIND SHOWING MY SILHOUETTE 
@@ -91,8 +95,8 @@ export default function Home() {
         <h1>Projects</h1>
         <div className="projects-container">
             <div class="container project-container">
-                <div class="row">
-                    <div class="col-6">
+                <div class="row justify-content-center">
+                    <div class="col-4">
                     <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">English in Eight Minutes</h5>
@@ -104,43 +108,47 @@ export default function Home() {
                         </div>
                     </div>
 
-                    <div class="col-6">
-                        <img src="../../images/english-in-eight.png" className="english-in-eight-image" alt="" />
+                    <div class="col-8">
+                        <Link to={{ pathname: "https://english-in-eight-minutes.web.app/" }} target="_blank" >
+                            <img src="../../images/english-in-eight.png" className="english-in-eight-image" />
+                        </Link>
                     </div> 
                 </div>
             </div>
             <div class="container project-container container-margin">
-            <div class="row">
-                <div class="col-6">
-                    <img src="../../images/dietrich-landcare.png" alt="" />
+                <div class="row">
+                    <div class="col-8">
+                        <img src="../../images/dietrich-landcare.png" alt="" />
+                    </div>
+                    <div class="col-4">
+                    <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Dietrich Landcare</h5>
+                                <p className="card-text">My son needed a website for his business, so I built him one</p>
+                            </div>
+                        </div>
+                    </div>  
                 </div>
-                <div class="col-6">
-                <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Dietrich Landcare</h5>
-                            <p className="card-text">My son needed a website for his business, so I built him one</p>
+            </div>
+            <div class="container project-container">
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                    <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">English in Eight Minutes</h5>
+                                <p className="card-text">So this is how it started. 
+                                I was building a English teaching website with the help of some students.
+                                I quickly became fascinated with coding and next realized I had found my
+                                first real professinal passion.</p>
+                            </div>
                         </div>
                     </div>
-                </div>  
-            </div>
-        </div>
-            <div class="container project-container container-margin">
-            <div class="row">
-                <div class="col-5">
-                <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">CGShop</h5>
-                            <p className="card-text">CGShop was a project I did in order to practice 
-                            my.......skills</p>
-                        </div>
-                    </div>
+
+                    <div class="col-8">
+                        <img src="../../images/cgshop.png" className="cgshop" alt="" />
+                    </div> 
                 </div>
-              
-                <div class="col-7">
-                    <img src="../../images/cgshop.png" className="english-in-eight-image" alt="" />
-                </div> 
             </div>
-        </div>
         </div>
         <GetInTouch/>
     </div>
