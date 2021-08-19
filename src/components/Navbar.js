cf768924e54171b8db053bb1ef37c83ca93a5cbe
Chris Dietrich <chrisdietrich3666@gmail.com>
2021-08-19 08:54:54 -0400

    deploying to firebase

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container">
-                <a className="navbar-brand" ><img src="/images/portfolio-logo.png" style={{width: "180px"}}/></a>
+                <a className="navbar-brand" ><img src="/images/logo.png" style={{width: "180px"}}/></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
